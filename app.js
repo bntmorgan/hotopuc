df90b2658400518a8d7b08a24d05a89b84e0fe8a
Benoît Morgan <bmorgan@laas.fr>
2015-05-01 19:17:24 +0200

    A lot off new stuff !

--- app.js
+++ app.js
@@ -17,6 +17,9 @@ var fs = require('fs')
 var v = false
 var verbose = function(s, l) {
   if (typeof(l)==='undefined') l = 'info';
+  if (l == 'debug') {
+    return
+  }
   if (v) {
     process.stdout.write('[' + l + '] ')
     console.log(s)
@@ -27,6 +30,13 @@ var error = function(s) {
   process.stdout.write('[error] ')
   console.log(s)
   process.kill()
+}
+
+function Rec(o) {
+  this.name = o.name || 'John Doe'
+  this.photos = o.photos || []
+  this.birth_date = o.birth_date || []
+  this.id = o.id || 0
 }
 
 function Match(o) {
@@ -92,11 +102,74 @@ function Tinder() {
 
   // Models
   this.matches = {}
+  this.recs = {}
 
   // Attributes
   this.v = false
 
+  this.getRecsInfo = function(cb, o) {
+    self.raw_recs.results.forEach(function(p) {
+      verbose('Id : ' + p._id, 'debug')
+      verbose('  name : ' + p.name, 'debug')
+      verbose('  birth_date : ' + p.birth_date, 'debug')
+      photos = []
+      p.photos.forEach(function(ph) {
+        verbose('  photo id : ' + ph.id, 'debug')
+        verbose('    photo url : ' + ph.url, 'debug')
+        photos.push(new Photo({'url': ph.url}))
+      })
+      self.recs[p._id] = new Rec({
+        'name': p.name,
+        'photos': photos,
+        'birth_date': p.birth_daye,
+        'id': p._id
+      })
+    })
+    cb()
+  }
+
   // Functions
+  this.getRecs = function(cb, o) {
+    var heads = extend({}, self.http_headers, {
+      'X-Auth-Token': self.access_token
+    })
+    var options = {
+      host: self.host,
+      port: '443',
+      path: '/recs',
+      method: 'GET',
+      headers: heads
+    }
+
+    // Set up the request
+    var get_req = https.request(options, function(res) {
+      res.setEncoding('utf8')
+      var r = ""
+      var end = function() {
+        var t = eval('[' + r + ']')[0]
+        if (t.results == undefined) {
+          self.raw_recs = {results: []}
+        } else {
+          self.raw_recs = t
+        }
+        // Call the callback
+        cb()
+      }
+      res.on('data', function (chunk) {
+        r += chunk
+      })
+      res.on('end', end)
+      res.on('close', end)
+    })
+
+    get_req.on('error', function(err) {
+      throw 'error opening file: ' + err;
+    })
+
+    // get the data
+    get_req.end()
+  }
+
   this.getUpdates = function(cb, o) {
     var post_data = JSON.stringify({'last_activity_date':''})
     var heads = extend({}, self.http_headers, {
@@ -142,25 +215,26 @@ function Tinder() {
   }
 
   this.getMatchesInfo = function(cb, o) {
+    matches = {}
     self.raw_matches.forEach(function(e) {
       var p = e.person
-      verbose('Id : ' + p._id)
-      verbose('  name : ' + p.name)
+      verbose('Id : ' + p._id, 'debug')
+      verbose('  name : ' + p.name, 'debug')
       photos = []
       p.photos.forEach(function(ph) {
-        verbose('  photo id : ' + ph.id)
-        verbose('    photo url : ' + ph.url)
+        verbose('  photo id : ' + ph.id, 'debug')
+        verbose('    photo url : ' + ph.url, 'debug')
         photos.push(new Photo({'url': ph.url}))
       })
       messages = []
       e.messages.forEach(function(m) {
-        verbose('  msg id : ' + m._id)
-        verbose('  msg from : ' + m.from)
-        verbose('    msg text : ' + m.message)
+        verbose('  msg id : ' + m._id, 'debug')
+        verbose('  msg from : ' + m.from, 'debug')
+        verbose('    msg text : ' + m.message, 'debug')
         messages.push(new Message({'m': m.message, 'from': m.from, 'to': m.to,
             'date': m.sent_date}))
       })
-      self.matches[p._id] = new Match({
+      matches[p._id] = new Match({
         'name': p.name,
         'photos': photos,
         'messages': messages,
@@ -168,7 +242,20 @@ function Tinder() {
         'mid': e._id
       })
     })
+    verbose('Matches before : ' + Object.keys(self.matches).length)
+    verbose('Matches after : ' + Object.keys(matches).length)
+    if (Object.keys(matches).length > Object.keys(self.matches).length) {
+      console.log('You have new likes !!!')
+    }
+    self.matches = matches
     cb()
+  }
+
+  this.getRec = function(id) {
+    if (self.recs[id] == undefined) {
+      error('Unexisting match')
+    }
+    return self.recs[id]
   }
 
   this.getMatch = function(id) {
@@ -184,9 +271,15 @@ function Tinder() {
       if (err) {
         throw 'error opening file: ' + err;
       }
-      verbose("Cache loaded")
       self.matches = eval('[' + b + ']')[0]
-      cb()
+      fs.readFile(".recsDB",  null, function(err, b) {
+        if (err) {
+          throw 'error opening file: ' + err;
+        }
+        verbose("Cache loaded")
+        self.recs = eval('[' + b + ']')[0]
+        cb()
+      })
     })
   }
 
@@ -197,8 +290,14 @@ function Tinder() {
       if (err) {
         throw 'error opening file: ' + err;
       }
-      verbose("Cache saved")
-      cb()
+      fs.writeFile(".recsDB", JSON.stringify(self.recs, null, 2),
+          function(err) {
+        if (err) {
+          throw 'error opening file: ' + err;
+        }
+        verbose("Cache saved")
+        cb()
+      })
     })
   }
 
@@ -262,6 +361,14 @@ function Tinder() {
     cb()
   }
 
+  // Print all recs
+  this.printAllRecs = function(cb, o) {
+    for (var k in self.recs) {
+      console.log('[' + k + ']: ' + self.recs[k].name)
+    }
+    cb()
+  }
+
   // Print all matches
   this.printAllMatches = function(cb, o) {
     for (var k in self.matches) {
@@ -270,7 +377,107 @@ function Tinder() {
     cb()
   }
 
-  this.printAllUrls = function(cb, o) {
+  this.extremMassLike = function(cb, o) {
+    var recs = []
+    for (var k in self.recs) {
+      recs.push(k)
+    }
+    var rc = 0
+    var lf = function() {
+      if (rc < recs.length) {
+        self.recLike(lf, {id: recs[rc]})
+        rc++
+      } else {
+        cb()
+      }
+    }
+    lf()
+  }
+  
+  // Recs match
+  this.recLike = function(cb, o) {
+
+    // Get the rec
+    r = self.getRec(o.id)
+
+    verbose('Linking : ' + r.name)
+
+    var heads = extend({}, self.http_headers, {
+      'X-Auth-Token': self.access_token
+    })
+    var options = {
+      host: self.host,
+      port: '443',
+      path: '/like/'+r.id,
+      method: 'GET',
+      headers: heads
+    }
+
+    // Set up the request
+    var get_req = https.request(options, function(res) {
+      res.setEncoding('utf8')
+      var r = ""
+      var end = function() {
+        var t = eval('[' + r + ']')[0]
+        verbose('Remaining likes : ' + t.likes_remaining)
+        // Call the callback
+        cb()
+      }
+      res.on('data', function (chunk) {
+        r += chunk
+      })
+      res.on('end', end)
+      res.on('close', end)
+    })
+
+    get_req.on('error', function(err) {
+      throw 'error opening file: ' + err;
+    })
+
+    // get the data
+    get_req.end()
+  }
+  
+  // Recs urls
+  this.printAllRecsUrls = function(cb, o) {
+    var recs = []
+    if (o.id != undefined) {
+      recs.push(self.getRec(o.id))
+    } else {
+      for (var k in self.recs) {
+        recs.push(self.recs[k])
+      }
+    }
+    fs.open(o.out, 'w', function(err, fd) {
+      if (err) {
+        throw 'error opening file: ' + err;
+      }
+      var b = new Buffer('#!/bin/bash\n')
+      fs.write(fd, b, 0, b.length, null, function() {
+        var wf = function(mc, pc) {
+          if (pc >= recs[mc].photos.length) {
+            mc++
+            pc = 0
+          }
+          if (mc < recs.length) {
+            var b = new Buffer('wget "' + recs[mc].photos[pc].url + '" -O ' +
+                recs[mc].name + '_' + pc + '.jpg\n')
+            fs.write(fd, b, 0, b.length, null, function(a, b , c) {
+              wf(mc, pc + 1)
+            })
+          } else {
+            fs.close(fd, function() {
+              cb()
+            })
+          }
+        }
+        wf(0, 0)
+      })
+    })
+  }
+
+  // Matches URLs
+  this.printAllMatchesUrls = function(cb, o) {
     var matches = []
     if (o.id != undefined) {
       matches.push(self.getMatch(o.id))
@@ -320,10 +527,14 @@ function Options() {
       ['v', 'verbose'             ,'Verbose output'],
       ['u', 'update'              ,'Recompute cache'],
       ['c', 'chick=ARG'           ,'Set chick id'],
+      ['L', 'like'                ,'Like the chick'],
       ['l', 'list'                ,'List all the matched chicks'],
+      ['r', 'recs'                ,'List all the recommended chicks'],
       ['m', 'message=ARG'         ,'Send a message to the current chick'],
       ['p', 'print'               ,'Print a matched chick'],
-      ['w', 'wget=ARG'            ,'Compute wget script and write to file'],
+      ['w', 'wget-matches=ARG'    ,'Compute wget script for match photo and write to file'],
+      ['W', 'wget-recs=ARG'       ,'Compute wget script for match photo and write to file'],
+      [null,'extrem-mass-like'    ,'Extrem mass like dude, the casual way'],
       ['h', 'help'                ,'display this help'],
       ['', 'version'              ,'show version']
     ])              // create Getopt instance
@@ -341,6 +552,7 @@ function Command(options) {
   this.f = options.f
   this.o = options.o
 
+  verbose(self.f)
   // Methods
   this.execute = function(cb) {
     self.f(cb, self.o)
@@ -354,7 +566,7 @@ function Pipeline(options, tinder) {
   this.options = options
   this.tinder = tinder
   this.p = []
-  this.matchId = null
+  this.chickId = null
 
   this.buildPipeline = function() {
     // Build the options
@@ -363,39 +575,60 @@ function Pipeline(options, tinder) {
       if (o.verbose) {
         v = true
       }
+      self.p.push(new Command({f: self.tinder.cacheLoad}))
       if (o.update) {
         self.p.push(new Command({f: self.tinder.getUpdates}))
         self.p.push(new Command({f: self.tinder.getMatchesInfo}))
-      } else {
-        self.p.push(new Command({f: self.tinder.cacheLoad}))
+        self.p.push(new Command({f: self.tinder.getRecs}))
+        self.p.push(new Command({f: self.tinder.getRecsInfo}))
       }
       if (o.chick) {
-        self.matchId = o.chick
-        verbose('Match id selected is : ' + self.matchId)
+        self.chickId = o.chick
+        verbose('Match id selected is : ' + self.chickId)
       }
       if (o.print) {
-        if (self.matchId == null) {
+        if (self.chickId == null) {
           error('Select your chick first')
         }
         self.p.push(new Command({f: self.tinder.printMatch, o: {id:
-            self.matchId}}))
+            self.chickId}}))
       }
       if (o.message) {
-        if (self.matchId == null) {
+        if (self.chickId == null) {
           error('Select your chick first')
         }
         self.p.push(new Command({f: self.tinder.messageSend, o: {id:
-            self.matchId, m: o.message}}))
+            self.chickId, m: o.message}}))
       }
       if (o.list) {
         self.p.push(new Command({f: self.tinder.printAllMatches}))
       }
-      if (o.wget) {
-        var opt = {out: o.wget}
-        if (self.matchId != null) {
-          opt.id = self.matchId
+      if (o.recs) {
+        self.p.push(new Command({f: self.tinder.printAllRecs}))
+      }
+      if (o['wget-recs']) {
+        var opt = {out: o['wget-recs']}
+        if (self.recId != null) {
+          opt.id = self.recId
         }
-        self.p.push(new Command({f: self.tinder.printAllUrls, o: opt}))
+        self.p.push(new Command({f: self.tinder.printAllRecsUrls, o: opt}))
+      }
+      if (o['wget-matches']) {
+        var opt = {out: o['wget-matches']}
+        if (self.chickId != null) {
+          opt.id = self.chickId
+        }
+        self.p.push(new Command({f: self.tinder.printAllMatchesUrls, o: opt}))
+      }
+      if (o.like) {
+        opt = {}
+        if (self.chickId != null) {
+          opt.id = self.chickId
+        }
+        self.p.push(new Command({f: self.tinder.recLike, o: opt}))
+      }
+      if (o['extrem-mass-like']) {
+        self.p.push(new Command({f: self.tinder.extremMassLike}))
       }
       // Everytime in the end
       self.p.push(new Command({f: self.tinder.cacheSave}))
@@ -408,6 +641,7 @@ function Pipeline(options, tinder) {
       if (self.p.length > 0) {
         self.p.shift().execute(e)
       } else {
+        verbose("Pipeline end")
         cb()
       }
     }
@@ -420,7 +654,7 @@ function main() {
   p = new Pipeline(new Options(), new Tinder())
   p.buildPipeline()
   p.execute(function() {
-    verbose("Pipeline end")
+    verbose('Goodbye')
   })
 }
 
